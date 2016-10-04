b6bc8cb2429ecd7ef5eb9e53dfcf79ff4097db3e
Dan Gibbons <dan.gibbons@rbi.co.uk>
2016-10-04 13:28:07 +0100

    Adding WCF process activation for app

--- manifests/windows/web/app1.pp
+++ manifests/windows/web/app1.pp
@@ -3,6 +3,10 @@
 # @author Dan
 class profile::windows::web::app1 {
 
+  windowsfeature { [ 'NET-WCF-HTTP-Activation45' ]:
+    ensure  => present,
+  }
+
   class { 'windows::web::config':
     site_def_name  => 'egi'
   }
