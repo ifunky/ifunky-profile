898608dfe4eeecdd6009b548421c7eae4d6f7c52
Dan <dan@ifunky.net>
2017-01-24 16:54:27 +0000

    Adding TeamCity agent class

--- manifests/teamcity/agent.pp
+++ manifests/teamcity/agent.pp
@@ -1,5 +1,11 @@
 # Profile to install a TeamCity agent (Windows or Linux)
 #
-class profile::teamcity::agent () {
+class profile::teamcity::agent (
+  $server_url = undef,
+) {
+
+  class { 'teamcity::agent':
+    teamcity_server_url => $server_url
+  }
 
 }
