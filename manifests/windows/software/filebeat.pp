524d22eca741bc3e8dab6940bca4ce3dd1d99195
Dan <dan@ifunky.net>
2017-02-10 09:30:07 +0000

    Using forge filebeat instead of choco

--- manifests/windows/software/filebeat.pp
+++ manifests/windows/software/filebeat.pp
@@ -1,13 +1,29 @@
 # Installs FileBeat (https://www.elastic.co/products/beats/filebeat)
 #
 # @author Dan
-class profile::windows::software::filebeat (
-  $version = '5.2.0'
-)
+class profile::windows::software::filebeat ()
 {
 
-  package { 'filebeat':
-    ensure          => $version,
-    provider        => 'chocolatey',
+  class { 'filebeat':
+    outputs => {
+      'elasticsearch' => {
+        'hosts' => [
+          'http://elasticsearch.logging.pra.rbxd.ds:80/elastic/',
+        ],
+        'index'       => 'packetbeat',
+        'cas'         => [
+          '/etc/pki/root/ca.pem',
+        ],
+      },
+    },
   }
+
+  class { 'filebeat::prospector':
+    paths    => [
+      'D:\RBI Application Logs\EG Exporter\WindowService\*.log',
+      'D:\RBI Application Logs\EG Exporter\ConsoleApp\*.log',
+    ],
+    doc_type => 'log',
+  }
+
 }
