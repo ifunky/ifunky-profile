78e31a222580da653089a01639ba630b3f704ae5
Dan <dan@ifunky.net>
2017-02-13 15:38:08 +0000

    Adding application base class Make filebeat more generic

--- manifests/windows/software/filebeat.pp
+++ manifests/windows/software/filebeat.pp
@@ -1,14 +1,24 @@
 # Installs FileBeat (https://www.elastic.co/products/beats/filebeat)
 #
 # @author Dan
-class profile::windows::software::filebeat ()
+class profile::windows::software::filebeat (
+  $application_name     = undef,
+  $log_path             = hiera('base::app_logs_path'),
+  $proxy_server         = hiera('base::proxy_server',''),
+  $elasticsearch_server = hiera('base::elasticsearch_server'),
+)
 {
+
+  validate_absolute_path($log_path, "ERROR: log_path must be a valid path")
+  if empty($application_name) {
+    fail("You must supply an applicaiton name")
+  }
 
   class { 'filebeat':
     outputs => {
       'elasticsearch' => {
         'hosts' => [
-          'http://elasticsearch.logging.pra.rbxd.ds:80/elastic/',
+          $elasticsearch_server,
         ],
       },
     },
@@ -17,7 +27,7 @@ class profile::windows::software::filebeat ()
       'to_files'  => true,
       'to_syslog' => false,
     },
-
+    proxy_address => $proxy_server
   }
 
   filebeat::prospector { 'log':
@@ -25,7 +35,7 @@ class profile::windows::software::filebeat ()
       'D:\RBILogs\*.log',
     ],
     doc_type => 'log',
-    fields   => {"applicationName" => elastic-search-loader}
+    fields   => {"applicationName" => $application_name}
   }
 
 }
